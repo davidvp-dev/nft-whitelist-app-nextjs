f8635dd82d012cdf74360e77334a8bf3e155f8fa
davidvp <villadavid.crypto@gmail.com>
2023-07-26 12:27:07 +0200

    useEffect() on main app to control Web3 state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
+import { useWeb3Store } from '../stores/web3Store'
 import '@/styles/globals.css'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+
+  const { changeAddress, changeChainId, setProvider } = useWeb3Store()
+  //const setProvider = useWeb3Store()
+  console.log(setProvider);
+  /** 
+   * useEffect code is executed after the component has rendered. 
+   * It runs after every renfer by default, but we can control it by providing a dependency array as the second argument
+  **/
+  useEffect(() => {
+    setProvider()
+    if (!window.ethereum) return
+
+    window.ethereum.on("accountsChanged", (acc) => {
+      changeAddress(acc[0]);
+    })
+
+    window.ethereum.on("chainChanged", (newChainId) => {
+      changeChainId(Number(newChainId));
+    })
+
+    return () => {
+    }
+  }, [])
+
   return (
     <Component {...pageProps} />
   )
