ead5a9d7666e17fd4f092692b61b78d004b582a6
davidvp <villadavid.crypto@gmail.com>
2023-07-26 20:13:30 +0200

    Add new font family to the front

--- components/Main.js
+++ components/Main.js
@@ -23,11 +23,11 @@ export default function Mint() {
                             className="rounded-xl"
                         />
                     </div>
-                    <h4 className="text-center text-xl my-6 font-mono">Mint a NFT from the Eyes of the Departed NFT Collection and join the community to receive special offerings.</h4>
-                    <h4 className="text-center text-xl my-6 font-mono">20 available!</h4>
+                    <h4 className="text-center text-xl my-6 font-primary">Mint a NFT from the Eyes of the Departed NFT Collection and join the community to receive special offerings.</h4>
+                    <h4 className="text-center text-xl my-6 font-primary">20 NFT Available</h4>
                     <button
                         type="button"
-                        className="rounded border-2 border-neutral-50 my-6 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-350 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                        className="rounded border-4 border-neutral-50 my-6 px-7 pb-[8px] pt-[10px] text-xl font-primary bg-blue-600"
                         data-te-ripple-init
                         data-te-ripple-color="light">
                         MINT
@@ -35,7 +35,7 @@ export default function Mint() {
                 </div>
                 <hr className="my-20 border-1"></hr>
                 <div className="text-white">
-                    <h2 className="text-center text-4xl my-32 font-bold">Check below if you are in the whitelist!</h2>
+                    <h2 className="text-center text-4xl my-32 font-primary">Check if you are in the Whitelist!</h2>
                 </div>
             </div>
         </div>
