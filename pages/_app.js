7d7833767a1eb6adb04702990a882a48bba8fa77
davidvp <villadavid.crypto@gmail.com>
2023-07-29 16:46:20 +0200

    Create and instance contract with Zustand

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { useWeb3Store } from '@/utils/ethers/web3Store'
+import { useContractStore } from '@/utils/ethers/contractStore';
+import { whitelistContract } from "@/constants/whitelist-abi";
 import '@/styles/globals.css'
-import { accessListify } from 'ethers'
-import { useEffect } from 'react'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+
 
 export default function App({ Component, pageProps }) {
 
-  const { changeAddress, changeChainId, setProvider, chainId, isConnected, errorMessage } = useWeb3Store()
-
+  const { changeAddress, changeChainId, setProvider, chainId, provider, address } = useWeb3Store()
+  const { setContract } = useContractStore()
+  //const [contract, setContract] = useState();
   /** 
    * useEffect code is executed after the component has rendered. 
    * It runs after every renfer by default, but we can control it by providing a dependency array as the second argument
@@ -23,9 +27,20 @@ export default function App({ Component, pageProps }) {
       changeChainId(Number(newChainId));
     })
 
+    const loadContract = async () => {
+      try {
+        const contract = new ethers.Contract(whitelistContract.address, whitelistContract.abi, provider?.getSigner());
+        setContract(contract);
+      } catch (error) {
+        console.error('Error loading contract', error);
+      }
+    };
+
+    loadContract();
+
     return () => {
     }
-  }, [chainId])
+  }, [chainId, address])
 
   return (
     <Component {...pageProps} />
